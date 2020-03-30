bad8ae62d2d8a3c971f0d7a0235484849bb84b8a
GoossensJord <Goossenjord@hotmail.com>
2020-03-30 16:33:41 +0200

    initializer added

---
+++ MAFDB/DAL/MeetAndFeedDBInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    class MeetAndFeedDBInitializer
+    {
+        private static bool _hasRunDuringApplicationExecution;
+
+        public static void Initialize(MeetAndFeedDbContext context, bool forceDropCreateDatabase = false)
+        {
+            if (!_hasRunDuringApplicationExecution)
+            {
+                if (forceDropCreateDatabase)
+                    context.Database.EnsureDeleted();
+
+                if (context.Database.EnsureCreated())
+                    Seed(context);
+
+                _hasRunDuringApplicationExecution = true;
+            }
+        }
+    }
+}
