8a0a2156a48cf683b6e643c5aab9388fa11dec16
Vincent de wit <vincentdew@hotmail.com>
2020-03-31 19:23:06 +0200

    eerste probeersel API

---
+++ MeetAndFeedAPI/MeetAndFeedAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MeetAndFeedAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
