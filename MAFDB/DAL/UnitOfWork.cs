f5888d73c4b1edbe3515e0e16ac4d46566f24037
Rijoeben <61225406+Rijoeben@users.noreply.github.com>
2020-03-31 17:15:28 +0200

    UnitOfWork en UserRepository added

---
+++ MAFDB/DAL/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class UnitOfWork
+    {
+        internal MeetAndFeedDbContext Context { get; }
+
+        public UnitOfWork(MeetAndFeedDbContext context)
+        {
+            Context = context;
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ MAFDB/DAL/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class UserRepository
+    {
+
+        private readonly MeetAndFeedDbContext ctx;
+
+        public UserRepository()
+        {
+            ctx = new MeetAndFeedDbContext();
+        }
+    }
+}
