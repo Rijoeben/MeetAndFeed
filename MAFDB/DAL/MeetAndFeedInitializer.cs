981f0a203cd71f452e83863431887d5336451d79
GoossensJord <Goossenjord@hotmail.com>
2020-03-31 15:10:02 +0200

    ruben

---
+++ MAFDB/DAL/MeetAndFeedInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MAFDB;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    class MeetAndFeedInitializer
+    {
+        private static bool _hasRunDuringApplicationExecution;
+
+        public static void Initialize(MeetAndFeedDbContext context, bool forceDropCreateDatabase = false)
+        {
+            if (!_hasRunDuringApplicationExecution)
+            {
+                if (forceDropCreateDatabase)
+                    context.Database.ens
+
+                if (context.Database.EnsureCreated())
+                    Seed(context);
+
+                _hasRunDuringApplicationExecution = true;
+            }
+        }
+    }
+}
--- MAFDB/DAL/Program.cs
+++ MAFDB/DAL/Program.cs
@@ -4,6 +4,9 @@ namespace DAL
 {
     class Program
     {
-
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
     }
 }
