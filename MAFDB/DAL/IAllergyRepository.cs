6baace38a61381751cc85a9ac21e9fe05137154d
Billybatzzbeer16 <62837618+Billybatzzbeer16@users.noreply.github.com>
2020-04-01 15:59:57 +0200

    Repositories afgewerkt

---
+++ MAFDB/DAL/IAllergyRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MAFDB;
+
+namespace DAL
+{
+    public interface IAllergyRepository
+    {
+        IEnumerable<Allergy> ReadAllergy();
+
+        Allergy CreateAllergy(Allergy allergy);
+
+        void UpdateAllergy(Allergy allergy);
+
+        void DeleteAllergy(Allergy allergy);
+    }
+}
---
+++ MAFDB/DAL/IReviewsRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MAFDB;
+
+namespace DAL
+{
+    class IReviewsRepository
+    {
+        public interface IReviewRepository
+        {
+            IEnumerable<Review> ReadReview();
+
+            Allergy CreateReview(Review review);
+
+            void UpdateReview(Review review);
+
+            void DeleteReview(Review review);
+        }
+    }
+}
