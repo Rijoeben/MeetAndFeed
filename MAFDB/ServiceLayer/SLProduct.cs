9dfb2175f9798980924d866ec34716cae297e6e4
GoossensJord <Goossenjord@hotmail.com>
2020-04-03 13:45:32 +0200

    undone changes

---
+++ MAFDB/ServiceLayer/SLProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using MAFDB;
+using DAL;
+
+
+namespace ServiceLayer
+{
+    public class SLProduct
+    {
+        public 
+    }
+}
