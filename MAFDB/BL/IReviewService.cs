9fc0cae00cfb12cf9d4852e67209c2355d9401c8
GoossensJord <Goossenjord@hotmail.com>
2020-04-04 14:58:41 +0200

    Review BL done

---
+++ MAFDB/BL/IReviewService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MAFDB;
+using DAL;
+namespace BL
+{
+    public interface IReviewService
+    {
+        public Review AddReview(string content);
+    }
+}
---
+++ MAFDB/BL/ReviewService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MAFDB;
+using DAL;
+
+namespace BL
+{
+    public class ReviewService : IReviewService
+    {
+        private IReviewRepository _repo;
+
+        public Review AddReview(string content)
+        {
+            Review newReview = new Review();
+
+            newReview.Content = content;
+
+            _repo.CreateReview(newReview);
+            return newReview;
+        }
+    }
+}
