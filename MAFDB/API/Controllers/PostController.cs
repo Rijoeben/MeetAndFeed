77ec7a593f4400e39d6f9644cca661d091da31a2
Jord Goossens <r0805959@student.thomasmore.be>
2020-04-29 09:18:11 +0200

    post api bijna af, nog een kleine correctie nodig.

--- MAFDB/API/Controllers/PostController.cs
+++ MAFDB/API/Controllers/PostController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using BL;
 using MAFDB;
@@ -26,11 +27,39 @@ namespace API.Controllers
             if (posts == null) return NotFound();
             return Ok(posts);
         }
-        [HttpPut]
-        public IActionResult PutPost(string dish, string description, DateTime date, int amountOfPeople)//long userId)
+        [HttpGet("{id}")]     /*!Vragen!*/
+        public IActionResult GetPostByID(long Id)
+        {
+            var post = _postService.ReadPost(Id);
+          
+            
+            return Ok(post);
+        }
+        [HttpPost]
+        public IActionResult AddingPost(string dish, string description, DateTime date, int amountOfPeople)//long userId)
         {
             var newPost = _postService.CreatePost(dish, description, date, amountOfPeople);
             return Ok(newPost);
         }
+        [HttpPut]
+        public IActionResult UpdatePost(string dish, string description, DateTime date, int amountOfPeople, long postId)
+        {
+            var postToChange = _postService.ReadPost(postId);
+            if(postToChange != null)
+            {
+                postToChange = _postService.ChangePost(dish, description, date, amountOfPeople, postId);
+
+                return Ok(postToChange);
+            }
+
+            return NotFound();
+        }
+        [HttpDelete]
+        public IActionResult DeletePost(long postId)
+        {
+            _postService.RemovePost(postId);
+            return Ok(postId);
+        }
+
     }
 }
